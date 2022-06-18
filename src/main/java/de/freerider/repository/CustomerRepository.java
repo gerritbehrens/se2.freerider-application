87685792a42a9da17316864a30b6807afc81cb4d
s50551 <s50551@beuth-hochschule.de>
2022-06-18 14:49:48 +0200

    Spring-Data-JPA Annotations

--- src/main/java/de/freerider/repository/CustomerRepository.java
+++ src/main/java/de/freerider/repository/CustomerRepository.java
@@ -3,6 +3,8 @@ package de.freerider.repository;
 import org.springframework.stereotype.Repository;
 import de.freerider.datamodel.Customer;
 
+import javax.persistence.Entity;
+
 @Repository
 public interface CustomerRepository extends
         org.springframework.data.repository.CrudRepository<Customer, Long>
